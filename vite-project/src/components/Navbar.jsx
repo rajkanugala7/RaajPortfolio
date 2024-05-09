c44c61de6fff0185a862ae51c706718a5358ce96
rajkanugala7 <rajkanugala7@gmail.com>
2024-05-09 22:33:01 +0530

    projects

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active" aria-current="page" href="/"><i class="fa-solid fa-house"></i>&nbsp;Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/about">About</a>
