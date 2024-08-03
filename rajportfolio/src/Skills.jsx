3640c5fc089bf6a1b4e234373452ecf88971167f
rajkanugala7 <rajkanugala7@gmail.com>
2024-08-03 12:48:19 +0530

    updated

--- rajportfolio/src/Skills.jsx
+++ rajportfolio/src/Skills.jsx
@@ -1,7 +1,91 @@
+
+
 export default function Skills() {
     return (
-        <div>
-            <h1>Skills</h1>
+        <div className="skills p-5">
+               <h1 className="skills-title p-3">Skills  </h1>
+              
+            <div className="skillsdiv mt-5">
+                <div className="row">
+                    <div className="col-1"></div>
+                    <div className="skill col-2 ">
+                    <h1 className="skill-logo"><i class="fa-brands fa-java"></i></h1>
+                    <h5 className="skill-info">DSA with Java</h5>
+                </div>
+                <div className="skill col-2">
+                    <h1 className="skill-logo react-logo"><i class="fa-brands fa-react"></i></h1>
+                    <h5 className="skill-info">React.js</h5>
+                </div>
+                 
+                <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-brands fa-js"></i></h1>
+                    <h5 className="skill-info">Javascript</h5>
+                    </div>
+                    <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-brands fa-node"></i></h1>
+                    <h5 className="skill-info">Node.js & Express.js</h5>
+                    </div>
+                    <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-solid fa-code"></i></h1>
+                    <h5 className="skill-info">coding in C/C++</h5>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-1"></div>
+                    <div className="skill col-2 ">
+                    <h1 className="skill-logo"><i class="fa-brands fa-html5"></i></h1>
+                    <h5 className="skill-info">HTML</h5>
+                </div>
+                <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-brands fa-css3"></i></h1>
+                    <h5 className="skill-info">CSS</h5>
+                </div>
+                <div className="skill col-2 ">
+                    <h1 className="skill-logo"><i class="fa-brands fa-github"></i></h1>
+                    <h5 className="skill-info">Git | GitHub</h5>
+                </div>
+                <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-solid fa-table"></i></h1>
+                    <h5 className="skill-info">SQL</h5>
+                    </div>
+                    <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-solid fa-database"></i></h1>
+                    <h5 className="skill-info">Mongodb</h5>
+                    </div>
+                         
+                </div>
+
+                <div className="row mt-5">
+                    <div className="col-2"></div>
+    
+                    <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-solid fa-handshake"></i></h1>
+                    <h5 className="skill-info">Team Work</h5>
+                    </div>
+                
+                    
+                    <div className="skill col-2 ">
+                    <h1 className="skill-logo"><i class="fa-brands fa-python"></i></h1>
+                    <h5 className="skill-info">Python</h5>
+                </div>
+                <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-brands fa-bootstrap"></i></h1>
+                    <h5 className="skill-info">Bootstrap</h5>
+                </div>
+                <div className="skill col-2">
+                    <h1 className="skill-logo"><i class="fa-brands fa-uikit"></i></h1>
+                    <h5 className="skill-info">MaterialUI</h5>
+                    </div>
+                    
+                
+                </div>
+                <div className="row mt-5">
+                    <div className="col-3"></div>
+                    
+                    
+                </div>
+
+                </div>
         </div>
     )
 }
